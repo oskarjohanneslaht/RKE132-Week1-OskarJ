725a841da9531114fb2900af0ade4db08df4b535
Oskar <oskar.johannes.laht@tktk.ee>
2023-04-26 13:29:09 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,19 @@
+﻿// See on kommentaar, mida ei kuvata konsoolil
+// Koodiriba saab kommenteerida
+// muutuja nimed peavad olema erinevad
+  
+// Rakendus küsib kasutajalt tema nime
+// Rakendus tervitade teda nimepidi
+
+    Console.WriteLine("Enter your name:"); //output
+
+    // string tähendab sõne
+    string userName = Console.ReadLine(); //input
+
+    Console.WriteLine("Hello" + userName); //output
+    //string interpolation 
+
+    Console.WriteLine($"Hello, {userName}!"); //output
+
+
+
